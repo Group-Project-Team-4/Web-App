13b2ad2a5fb67fcaadc68925a4c9d06d131ba8aa
Dalton <contact@daltonp.dev>
2023-10-30 12:50:12 -0400

    feat: select products and product categories from database

--- clothing_store/shop.py
+++ clothing_store/shop.py
@@ -1,7 +1,21 @@
 from flask import Blueprint, render_template
 
+from clothing_store.db import get_db
+
 bp = Blueprint("shop", __name__)
+
 
 @bp.route("/shop")
 def index():
-    return render_template("shop/index.html")
+    db = get_db()
+    products = db.execute(
+        "SELECT id, name, price, category_id" " FROM product"
+    ).fetchall()
+
+    product_categories = db.execute(
+        "SELECT id, name" " FROM product_category"
+    ).fetchall()
+
+    return render_template(
+        "shop/index.html", products=products, product_categories=product_categories
+    )
