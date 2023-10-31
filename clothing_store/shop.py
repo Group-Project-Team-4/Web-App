52b7c05c4132d81497cba9b8cf69d1a18ab8324a
Dalton <contact@daltonp.dev>
2023-10-30 20:20:50 -0400

    feat: implemented category filtering for products
    Category filtering is done through a form with a select input that uses the category id for the value. The form auto submits when the user changes the category. Then the flask application selects the products based on this category filter.

--- clothing_store/shop.py
+++ clothing_store/shop.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from clothing_store.db import get_db
 
@@ -7,11 +7,24 @@ bp = Blueprint("shop", __name__)
 
 @bp.route("/shop")
 def index():
-    db = get_db()
-    products = db.execute(
-        "SELECT id, name, price, category_id" " FROM product"
-    ).fetchall()
+    # Get the category_id from the request parameters
+    category_id = request.args.get("category", default="All", type=str)
 
+    db = get_db()
+
+    # Get all products if category_id is "All", otherwise get products in the specified category
+    products = None
+    if category_id != "All":
+        products = db.execute(
+            "SELECT id, name, price, category_id" " FROM product WHERE category_id = ?",
+            (category_id),
+        ).fetchall()
+    else:
+        products = db.execute(
+            "SELECT id, name, price, category_id" " FROM product"
+        ).fetchall()
+
+    # Get all product categories
     product_categories = db.execute(
         "SELECT id, name" " FROM product_category"
     ).fetchall()
