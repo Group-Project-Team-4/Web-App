aca0308e80afee7a8b9b8ae2e8efd2f4556b7798
Moonlight1211 <magtronian@gmail.com>
2023-11-05 17:41:54 -0500

    feat: Updated store.py to make the / route return the home page template

--- clothing_store/store.py
+++ clothing_store/store.py
@@ -4,9 +4,8 @@ from werkzeug.exceptions import abort
 from clothing_store.auth import login_required
 from clothing_store.db import get_db
 
-bp = Blueprint("blog", __name__)
+bp = Blueprint("home", __name__)
 
 @bp.route("/")
 def index():
-    return "<h1>It works!</h1>\
-            The app is running, but this home page is WIP. If you are testing another page, navigate to it manually via the URL."
+    return render_template('home/index.html')
