88edae41eadf9139ac20a8ebb3464a3532e65926
Dalton <contact@daltonp.dev>
2023-11-05 20:17:59 -0500

    feat: added post request to /cart route for handling add to cart
    The post request adds an item to the cart if it's not already in it and adds one the the quantity if it is.

--- clothing_store/cart.py
+++ clothing_store/cart.py
@@ -1,11 +1,29 @@
-from flask import Blueprint, render_template, g, redirect, url_for
+from flask import (
+    Blueprint,
+    render_template,
+    g,
+    redirect,
+    url_for,
+    request,
+    flash,
+    abort,
+)
 
 from clothing_store.db import get_db
 
 bp = Blueprint("cart", __name__)
 
 
-@bp.route("/cart")
+def select_product(product_id):
+    db = get_db()
+    product = db.execute(
+        "SELECT id, name, price, quantity FROM product WHERE id = ?", (
+            product_id,)
+    ).fetchone()
+    return product
+
+
+@bp.route("/cart", methods=("GET", "POST"))
 def index():
     # redirect to login page if user is not logged in
     if g.user is None:
@@ -16,13 +34,66 @@ def index():
     # get all items in the cart for the current user
     cart = db.execute(
         """
-        SELECT cart_item.id, product_id, user_id, quantity, name, price
+        SELECT cart_item.id, product_id, user_id, cart_item.quantity, name, price
         FROM cart_item
         JOIN product ON product_id = product.id
         WHERE user_id = ?
         """,
         (str(g.user["id"])),
     ).fetchall()
+
+    # post method for adding items to cart
+    if request.method == "POST":
+        product_id = None
+        quantity = None
+
+        # validate product_id and quantity are provided in request
+        try:
+            product_id = request.form["product_id"]
+        except KeyError:
+            abort(400, "Product ID is required.")
+
+        try:
+            quantity = request.form["quantity"]
+        except KeyError:
+            abort(400, "Quantity is required.")
+
+        # validate product_id (check if product_id exists in database)
+        product = select_product(product_id)
+
+        if product is None:
+            abort(400, "Product does not exist.")
+
+        # check if item already exists in cart
+        is_in_cart = False
+        cart_item_quantity = 0
+        for item in cart:
+            if item["product_id"] == int(product_id):
+                is_in_cart = True
+                cart_item_quantity = item["quantity"]
+                break
+
+        # if product quantity is exceeded, flash error and redirect to previous page
+        if product["quantity"] < cart_item_quantity + 1:
+            flash("Product quantity exceeded.")
+            return redirect(request.referrer)
+
+        if is_in_cart:
+            # update quantity of item in cart
+            db.execute(
+                "UPDATE cart_item SET quantity = quantity + 1 WHERE product_id = ? AND user_id = ?",
+                (product_id, str(g.user["id"])),
+            )
+            db.commit()
+        else:
+            # add item to cart if it does not exist
+            db.execute(
+                "INSERT INTO cart_item (product_id, user_id, quantity) VALUES (?, ?, ?)",
+                (product_id, str(g.user["id"]), quantity),
+            )
+            db.commit()
+
+        return redirect(url_for("cart.index"), code=302)
 
     # calculate total price of all items in the cart
     total_price = 0
