a7898917e0ac608a4a8fecb9034247fb3e5eaa47
Dalton <contact@daltonp.dev>
2023-11-05 20:06:34 -0500

    feat: created user_id and product_id constants
    Created constants for user_id and product_id so that they are not magic numbers.

--- tests/pytest/test_cart.py
+++ tests/pytest/test_cart.py
@@ -12,13 +12,17 @@ def test_cart(client, app, auth):
     assert client.get("/cart").status_code == 200
 
 
+# id of product being tested
+PRODUCT_ID = 3
+USER_ID = 1
+
 def test_add_to_cart(client, app, auth):
     # test add to cart responds with 401 if user is not logged in
     assert client.post("/cart").status_code == 401
 
     # # test add to cart responds with 302 and redirects to cart page if user is logged in
     auth.login()
-    response = client.post("/cart", data={"product_id": 3, "quantity": 1})
+    response = client.post("/cart", data={"product_id": PRODUCT_ID, "quantity": 1})
     print(response)
     assert response.status_code == 302
     assert response.headers["Location"] == "/cart"
@@ -28,21 +32,21 @@ def test_add_to_cart(client, app, auth):
         assert (
             get_db()
             .execute(
-                "SELECT * FROM cart_item WHERE user_id = '1' AND product_id = '3'",
+                f"SELECT * FROM cart_item WHERE user_id = '{USER_ID}' AND product_id = '{PRODUCT_ID}'",
             )
             .fetchone()
             is not None
         )
 
     # # test add to cart increases quantity if product is already in cart
-    response = client.post("/cart", data={"product_id": 3, "quantity": 1})
+    response = client.post("/cart", data={"product_id": PRODUCT_ID, "quantity": 1})
     assert response.status_code == 302
     assert response.headers["Location"] == "/cart"
     with app.app_context():
         assert (
             get_db()
             .execute(
-                "SELECT * FROM cart_item WHERE user_id = '1' AND product_id = '3'",
+                f"SELECT * FROM cart_item WHERE user_id = '{USER_ID}' AND product_id = '{PRODUCT_ID}'",
             )
             .fetchone()["quantity"]
             == 2
@@ -50,13 +54,13 @@ def test_add_to_cart(client, app, auth):
 
     # test cart quantity doesn't exceed quantity in stock when adding to cart
     auth.login()
-    response = client.post("/cart", data={"product_id": 3, "quantity": 100})
+    response = client.post("/cart", data={"product_id": PRODUCT_ID, "quantity": 100})
     assert response.status_code == 302
     with app.app_context():
         assert (
             get_db()
             .execute(
-                "SELECT * FROM cart_item WHERE user_id = '1' AND product_id = '3'",
+                f"SELECT * FROM cart_item WHERE user_id = '{USER_ID}' AND product_id = '{PRODUCT_ID}'",
             )
             .fetchone()["quantity"]
             == 2
@@ -70,7 +74,7 @@ def test_add_to_cart(client, app, auth):
         # test add to cart responds with error message if product_id is not provided
         ({"quantity": 1}, b"Product ID is required."),
         # test add to cart responds with error message if quantity is not provided
-        ({"product_id": 1}, b"Quantity is required."),
+        ({"product_id": PRODUCT_ID}, b"Quantity is required."),
         # test add to cart responds with 404 if product does not exist
         ({"product_id": 100, "quantity": 1}, b"Product does not exist."),
     ),
